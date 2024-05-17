b0639e3b00257b0e8508aeb572b44448dc092983
Maxwell Hanson <Mhanson@wgeld.org>
2024-05-17 16:46:52 -0400

    first ever push to branch5_merge2

--- Git_Playground/obj/Debug/net8.0/Git_Playground.AssemblyInfo.cs
+++ Git_Playground/obj/Debug/net8.0/Git_Playground.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02bde6d698b66516a00b7fafbc92ed59e2b87900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f520613e8c7dba304b730cd519595ea93059f3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
