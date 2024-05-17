cbeaf1a7bbef6c8fbddbfcbaa2b4f75d4880c5a0
Maxwell Hanson <Mhanson@wgeld.org>
2024-05-17 15:36:47 -0400

    branch1 and branch2 are deliberately similar, but I added an image to branch2 page for distinction

--- Git_Playground/obj/Debug/net8.0/Git_Playground.AssemblyInfo.cs
+++ Git_Playground/obj/Debug/net8.0/Git_Playground.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70db3e5d9cd52d94bdffb27012b5219f3abca50e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60f16b2b15fc1e1f60192dba2cbc088a59c90d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
