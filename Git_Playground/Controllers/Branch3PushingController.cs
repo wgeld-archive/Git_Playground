711534240cb868ddc1f9ee71e25ede2aa5cb21fe
Maxwell Hanson <Mhanson@wgeld.org>
2024-05-17 12:07:13 -0400

    1- First ever push to branch 3. Removed branch 2 content and replaced with branch 3 content about pushing

--- Git_Playground/Controllers/Branch3PushingController.cs
+++ Git_Playground/Controllers/Branch3PushingController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Git_Playground.Controllers;
 
-public class Branch2Controller : Controller
+public class Branch3PushingController : Controller
 {
     // GET
     public IActionResult Index()
