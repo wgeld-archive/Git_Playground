02bde6d698b66516a00b7fafbc92ed59e2b87900
Maxwell Hanson <Mhanson@wgeld.org>
2024-05-17 16:37:09 -0400

    second push to branch4

--- Git_Playground/obj/Debug/net8.0/Git_Playground.AssemblyInfo.cs
+++ Git_Playground/obj/Debug/net8.0/Git_Playground.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706865a0d63ab2f2b05814103a9f645e0d66bb14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb1c7f3b44cc204bf8f33c0bf88de4125e24558")]
 [assembly: System.Reflection.AssemblyProductAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
