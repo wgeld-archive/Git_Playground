706865a0d63ab2f2b05814103a9f645e0d66bb14
Maxwell Hanson <Mhanson@wgeld.org>
2024-05-17 16:15:18 -0400

    improvements to branch 3 page

--- Git_Playground/obj/Debug/net8.0/Git_Playground.AssemblyInfo.cs
+++ Git_Playground/obj/Debug/net8.0/Git_Playground.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7530794d7cd91a00906be1f7276d2fb89d6e76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37de37ba54a39fd911bc6428898b4874d0ca1ec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Git_Playground")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
